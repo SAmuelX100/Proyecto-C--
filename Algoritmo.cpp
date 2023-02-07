70b9bca6078198a377821461d441eacf46ee9b5a
SAmuelX100 <samuel118.sainthilaire@gmail.com>
2023-02-07 08:33:10 -0400

    Comments added

--- Algoritmo.cpp
+++ Algoritmo.cpp
@@ -10,13 +10,17 @@ int main()
     int definirSizeArray;
     int i, j, aux;
 
+    // Declaracion del tamano del array.
     cout << " >> Ingrese el tamano del array: ";
     cin >> definirSizeArray;
 
+    //Declaracion del array utilizando el auxiliar de tamano
     int numerosPrueba[definirSizeArray];
 
+    //Funcion Srand.
     srand(time(NULL));
 
+    //Este ciclo for se utiliza para iterar el array e ir rellenando sus posiciones con numeros random
     for (int m = 0; m < definirSizeArray; m++){
         int numeroRandom = rand()%definirSizeArray;
         numerosPrueba[m] = numeroRandom;
@@ -27,7 +31,7 @@ int main()
     // Hace una referencia al tamaño del Array, haciendo más fácil trabajar con este
     int tamanoArrayAux = sizeof(numerosPrueba) / sizeof(int);
 
-    cout << " >> Array sin ordenar: " << endl;
+    cout << " >> Array sin ordenar: " << endl; // Simple letrero
 
     for (i = 0; i < tamanoArrayAux; i++) // Ciclo for para mostrar el array desordenado
     {
@@ -54,6 +58,7 @@ int main()
 
     cout << " >> Imprimiendo Array ordernado: " << endl;
 
+    //Ciclo For para imprimir el array ya ordenado
     for (i = 0; i < tamanoArrayAux; i++)
     {
         cout << "  >> " << numerosPrueba[i] << endl;
